0636529afd1f9240939d8b6cef634ad891beef60
Ravi Pampana <rpampana@invent.org>
2023-10-01 17:26:19 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+import yfinance as yf
+import pandas as pd
+
+#df = pd.read_csv('company_names.csv')
+#df['company_name'] = df['company_name'].str.lower()
+app = FastAPI()
+
+@app.get("/stockprice/{company_name}")
+async def root(company_name : str):
+    ticker = yf.Ticker(company_name).info
+    market_price = ticker['currentPrice']
+    company_name = ticker['longName']
+    #previous_close_price = ticker['regularMarketPreviousClose']
+    return {"Stock price of "+company_name+" is": market_price}
+
+if __name__ == "__main__":
+    import uvicorn
+
+    # Run the FastAPI app with uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
